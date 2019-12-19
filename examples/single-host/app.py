7da62fe7944c7b85fc07bf686b9351483773a7f5
iromli <isman.firmansyah@gmail.com>
2019-12-20 03:54:35 +0700

    proper compose subcommand execution

--- examples/single-host/app.py
+++ examples/single-host/app.py
@@ -4,6 +4,7 @@ import io
 import ipaddress
 import json
 import os
+import pathlib
 import re
 import socket
 import time
@@ -11,8 +12,10 @@ import time
 import click
 import stdiomask
 from compose.cli.command import get_project
+from compose.cli.command import get_config_path_from_options
 from compose.cli.main import TopLevelCommand
 from compose.config.config import yaml
+from compose.config.environment import Environment
 from docker.types import HostConfig
 
 CONFIG_DIR = "volumes/config-init/db"
@@ -231,7 +234,34 @@ class App(object):
 
     def __init__(self):
         self.settings = self.get_settings()
-        self.tlc = TopLevelCommand(get_project("."), {})
+        self.touch_files()
+
+    @contextlib.contextmanager
+    def top_level_cmd(self):
+        try:
+            compose_files = self.get_compose_files()
+            config_path = get_config_path_from_options(
+                ".",
+                {},
+                {"COMPOSE_FILE": compose_files},
+            )
+
+            os.environ["COMPOSE_FILE"] = compose_files
+            os.environ["PERSISTENCE_TYPE"] = self.settings.get("PERSISTENCE_TYPE")
+            os.environ["PERSISTENCE_LDAP_MAPPING"] = self.settings.get("PERSISTENCE_LDAP_MAPPING")
+            os.environ["COUCHBASE_USER"] = self.settings.get("COUCHBASE_USER")
+            os.environ["COUCHBASE_URL"] = self.settings.get("COUCHBASE_URL")
+            os.environ["DOMAIN"] = self.settings.get("DOMAIN")
+            os.environ["HOST_IP"] = self.settings.get("HOST_IP")
+
+            env = Environment()
+            env.update(os.environ)
+
+            project = get_project(".", config_path, environment=env)
+            tlc = TopLevelCommand(project)
+            yield tlc
+        except Exception:
+            pass
 
     def get_settings(self):
         """Get merged settings (default and custom settings from local Python file).
@@ -250,23 +280,6 @@ class App(object):
         settings.update(custom_settings)
         return settings
 
-    @contextlib.contextmanager
-    def compose_env(self):
-        """Set environment variables passed to ``docker-compose`` command.
-        """
-        try:
-            # set env vars
-            os.environ["COMPOSE_FILE"] = self.get_compose_files()
-            os.environ["PERSISTENCE_TYPE"] = self.settings.get("PERSISTENCE_TYPE")
-            os.environ["PERSISTENCE_LDAP_MAPPING"] = self.settings.get("PERSISTENCE_LDAP_MAPPING")
-            os.environ["COUCHBASE_USER"] = self.settings.get("COUCHBASE_USER")
-            os.environ["COUCHBASE_URL"] = self.settings.get("COUCHBASE_URL")
-            os.environ["DOMAIN"] = self.settings.get("DOMAIN")
-            os.environ["HOST_IP"] = self.settings.get("HOST_IP")
-            yield
-        except Exception as exc:
-            raise exc
-
     def get_compose_files(self):
         files = ["docker-compose.yml"]
         for svc, filename in self.compose_mappings.items():
@@ -275,10 +288,9 @@ class App(object):
         return ":".join(files)
 
     def logs(self, follow, tail, services=None):
-        services = services or []
-        with self.compose_env():
-            self.tlc.logs({
-                "SERVICE": " ".join(services),
+        with self.top_level_cmd() as tlc:
+            tlc.logs({
+                "SERVICE": " ".join(services or []),
                 "--tail": tail,
                 "--follow": follow,
                 "--timestamps": False,
@@ -286,8 +298,8 @@ class App(object):
             })
 
     def config(self):
-        with self.compose_env():
-            self.tlc.config({
+        with self.top_level_cmd() as tlc:
+            tlc.config({
                 "--resolve-image-digests": False,
                 "--quiet": False,
                 "--services": False,
@@ -297,20 +309,17 @@ class App(object):
             })
 
     def down(self):
-        with self.compose_env():
-            self.tlc.down({
+        with self.top_level_cmd() as tlc:
+            tlc.down({
                 "--rmi": False,
                 "--volumes": False,
                 "--remove-orphans": True,
             })
 
     def _up(self, services=None):
-        services = services or []
-
-        with self.compose_env():
-            # TODO: docker.override.yml still loaded
-            self.tlc.up({
-                "SERVICE": services,
+        with self.top_level_cmd() as tlc:
+            tlc.up({
+                "SERVICE": services or [],
                 "--no-deps": False,
                 "--always-recreate-deps": False,
                 "--abort-on-container-exit": False,
@@ -329,17 +338,19 @@ class App(object):
 
         # suppress output of `ps` command
         with contextlib.redirect_stdout(trap):
-            self.tlc.ps({
-                "--quiet": True,
-                "--services": False,
-                "--all": False,
-                "SERVICE": [service],
-            })
+            with self.top_level_cmd() as tlc:
+                tlc.ps({
+                    "--quiet": True,
+                    "--services": False,
+                    "--all": False,
+                    "SERVICE": [service],
+                })
         return trap.getvalue().strip()
 
     @property
     def network_name(self):
-        return f"{self.tlc.project.name}_default"
+        with self.top_level_cmd() as tlc:
+            return f"{tlc.project.name}_default"
 
     def gather_ip(self):
         ip = ""
@@ -430,42 +441,43 @@ class App(object):
         return params
 
     def prepare_config_secret(self):
-        if not self.ps("consul"):
-            self._up(["consul"])
+        with self.top_level_cmd() as tlc:
+            if not self.ps("consul"):
+                self._up(["consul"])
 
-        if not self.ps("vault"):
-            self._up(["vault"])
+            if not self.ps("vault"):
+                self._up(["vault"])
 
-        secret = Secret(self.tlc.project.client)
-        secret.setup()
+                secret = Secret(tlc.project.client)
+                secret.setup()
 
-        # check if config exists
-        config = Config(self.tlc.project.client)
+            # check if config exists
+            config = Config(tlc.project.client)
 
-        hostname = config.hostname_from_backend()
-        if hostname:
-            self.settings["DOMAIN"] = hostname
-            return
-
-        click.echo("[W] Configuration not found in Consul")
-
-        if os.path.isfile(f"{CONFIG_DIR}/config.json"):
-            if click.confirm("Load previously saved configuration in local disk?", default=True):
-                hostname = config.hostname_from_file(f"{CONFIG_DIR}/config.json")
+            hostname = config.hostname_from_backend()
+            if hostname:
                 self.settings["DOMAIN"] = hostname
-                self.run_config_init()
                 return
 
-        # prompt inputs for generating new config and secret
-        if not hostname and not os.path.isfile("generate.json"):
-            params = self.generate_params("generate.json")
-            self.settings["DOMAIN"] = params["hostname"]
+            click.echo("[W] Configuration not found in Consul")
 
-        self.run_config_init(True)
+            if os.path.isfile(f"{CONFIG_DIR}/config.json"):
+                if click.confirm("Load previously saved configuration in local disk?", default=True):
+                    hostname = config.hostname_from_file(f"{CONFIG_DIR}/config.json")
+                    self.settings["DOMAIN"] = hostname
+                    self.run_config_init()
+                    return
 
-        # cleanup
-        with contextlib.suppress(FileNotFoundError):
-            os.unlink("generate.json")
+            # prompt inputs for generating new config and secret
+            if not hostname and not os.path.isfile("generate.json"):
+                params = self.generate_params("generate.json")
+                self.settings["DOMAIN"] = params["hostname"]
+
+            self.run_config_init(True)
+
+            # cleanup
+            with contextlib.suppress(FileNotFoundError):
+                pathlib.Path("generate.json").unlink()
 
     def run_config_init(self, generate=False):
         workdir = os.path.abspath(os.path.dirname(__file__))
@@ -478,33 +490,123 @@ class App(object):
         if generate:
             volumes.append(f"{workdir}/generate.json:/app/db/generate.json")
 
-        try:
-            cid = self.tlc.project.client.create_container(
-                image=f"gluufederation/config-init:{self.settings['CONFIG_INIT_VERSION']}",
-                name="config-init",
-                command="load",
-                environment={
-                    "GLUU_CONFIG_CONSUL_HOST": "consul",
-                    "GLUU_SECRET_VAULT_HOST": "vault",
-                },
-                host_config=HostConfig(
-                    version="1.25",
-                    network_mode=self.network_name,
-                    binds=volumes,
-                ),
-            ).get("Id")
+        with self.top_level_cmd() as tlc:
+            try:
+                cid = tlc.project.client.create_container(
+                    image=f"gluufederation/config-init:{self.settings['CONFIG_INIT_VERSION']}",
+                    name="config-init",
+                    command="load",
+                    environment={
+                        "GLUU_CONFIG_CONSUL_HOST": "consul",
+                        "GLUU_SECRET_VAULT_HOST": "vault",
+                    },
+                    host_config=HostConfig(
+                        version="1.25",
+                        network_mode=self.network_name,
+                        binds=volumes,
+                    ),
+                ).get("Id")
 
-            self.tlc.project.client.start(cid)
-            for log in self.tlc.project.client.logs(cid, stream=True):
-                click.echo(log.strip())
-        except Exception:
-            raise
-        finally:
-            self.tlc.project.client.remove_container(cid, force=True)
+                tlc.project.client.start(cid)
+                for log in tlc.project.client.logs(cid, stream=True):
+                    click.echo(log.strip())
+            except Exception:
+                raise
+            finally:
+                tlc.project.client.remove_container(cid, force=True)
 
     def up(self):
         self.gather_ip()
         self.prepare_config_secret()
+        self._up()
+        self.init_db_entries()
+        self.healthcheck()
+
+    def healthcheck(self):
+        # TODO: progressbar
+        import requests
+        import urllib3
+        urllib3.disable_warnings()
+
+        click.echo("[I] Launching; please wait ...")
+
+        elapsed = 0
+        while elapsed <= 300:
+            with contextlib.suppress(requests.exceptions.ConnectionError):
+                req = requests.head(
+                    f"https://{self.settings['HOST_IP']}",
+                    verify=False,
+                )
+                if req.ok:
+                    click.echo(f"[I] Gluu Server installed successfully; please visit https://{self.settings['DOMAIN']}")
+                    break
+
+            time.sleep(5)
+            elapsed += 5
+
+    def touch_files(self):
+        files = [
+            "vault_role_id.txt",
+            "vault_secret_id.txt",
+            "gcp_kms_stanza.hcl",
+            "gcp_kms_creds.json",
+            "couchbase.crt",
+            "couchbase_password",
+            "casa.json",
+        ]
+        for file_ in files:
+            pathlib.Path(file_).touch()
+
+    def init_db_entries(self):
+        if os.path.isfile("db_initialized"):
+            return
+
+        click.echo("[I] Adding entries to database")
+
+        workdir = os.path.abspath(os.path.dirname(__file__))
+
+        volumes = [
+            f"{workdir}/vault_role_id.txt:/etc/certs/vault_role_id",
+            f"{workdir}/vault_secret_id.txt:/etc/certs/vault_secret_id",
+            f"{workdir}/couchbase.crt:/etc/certs/couchbase.crt",
+            f"{workdir}/couchbase_password:/etc/gluu/conf/couchbase_password",
+        ]
+
+        with self.top_level_cmd() as tlc:
+            try:
+                cid = tlc.project.client.create_container(
+                    image=f"gluufederation/persistence:{self.settings['PERSISTENCE_VERSION']}",
+                    name="persistence",
+                    environment={
+                        "GLUU_CONFIG_CONSUL_HOST": "consul",
+                        "GLUU_SECRET_VAULT_HOST": "vault",
+                        "GLUU_PERSISTENCE_TYPE": self.settings["PERSISTENCE_TYPE"],
+                        "GLUU_PERSISTENCE_LDAP_MAPPING": self.settings["PERSISTENCE_LDAP_MAPPING"],
+                        "GLUU_LDAP_URL": "ldap:1636",
+                        "GLUU_COUCHBASE_URL": self.settings["COUCHBASE_URL"],
+                        "GLUU_COUCHBASE_USER": self.settings["COUCHBASE_USER"],
+                        "GLUU_OXTRUST_API_ENABLED": self.settings["OXTRUST_API_ENABLED"],
+                        "GLUU_OXTRUST_API_TEST_MODE": self.settings["OXTRUST_API_TEST_MODE"],
+                        "GLUU_PASSPORT_ENABLED": self.settings["PASSPORT_ENABLED"],
+                        "GLUU_CASA_ENABLED": self.settings["CASA_ENABLED"],
+                        "GLUU_RADIUS_ENABLED": self.settings["RADIUS_ENABLED"],
+                        "GLUU_SAML_ENABLED": self.settings["SAML_ENABLED"],
+                    },
+                    host_config=HostConfig(
+                        version="1.25",
+                        network_mode=self.network_name,
+                        binds=volumes,
+                    ),
+                ).get("Id")
+
+                tlc.project.client.start(cid)
+                for log in tlc.project.client.logs(cid, stream=True):
+                    click.echo(log.strip())
+                pathlib.Path("db_initialized").touch()
+            except Exception:
+                raise
+            finally:
+                tlc.project.client.remove_container(cid, force=True)
 
 
 @click.group()
